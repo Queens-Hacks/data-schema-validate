a9b4b231d0d1c7187f4fa62f63446d88a09232d7
Phil Schleihauf <uniphil@gmail.com>
2013-12-17 17:29:16 -0500

    print traces on failure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rx-data-validate',
-      version='0.0.3',
+      version='0.1.0',
       author='Philip Schleihauf',
       author_email='uniphil@gmail.com',
       license='MIT',
